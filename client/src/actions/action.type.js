5448cf023b5166b5fcf60e230f02ae761bdb2829
Duc Hoang <davishoang20@gmail.com>
2022-04-19 23:14:16 +0700

    delete unnecessary files

--- client/src/actions/action.type.js
+++
@@ -1,6 +0,0 @@
-export const register_success = "register_success";
-export const register_fail = "register_fail";
-export const login_succes = "login_success";
-export const login_fail = "register_fail";
-export const set_message = "set_message";
-export const clear_message = "clear_message";
--- client/src/components/Room/MessageList.js
+++
@@ -1,7 +0,0 @@
-
-
-function MessageList() {
-  
-}
-
-export default MessageList;
