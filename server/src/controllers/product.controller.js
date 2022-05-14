e2cbc6e4a4b3d586d9c5eaff605bf2d813d4df60
XanMarta <gmxancrazy24114@gmail.com>
2022-05-14 17:09:35 +0700

    fix buy api

--- server/src/controllers/product.controller.js
+++ server/src/controllers/product.controller.js
@@ -2,10 +2,6 @@ const { Product, Category } = require("../models/")
 const validation = require("../validations/product.validation")
 
 
-var lastquerry = []
-var selectedProduct = null
-
-
 const getProduct = async (req, res) => {
     const products = await Product.get()
     res.send(products)
@@ -36,34 +32,15 @@ const queryProduct = async (req, res) => {
         if (result.length == 0) {
             result = await Product.find_word(req.body.query)
         }
-        lastquerry = result
         res.send(result)
     }
 }
 
 
-const selectProduct = async (req, res) => {
-    if (!req.body.index) {
-        res.status(400).send("Request invalid")
-    } else {
-        let index = parseInt(req.body.index, 10)
-        if (index > lastquerry.length) {
-            res.status(400).send("Invalid product")
-        } else {
-            selectedProduct = lastquerry[index - 1]
-            res.send(selectedProduct)
-        }
-    }
-}
-
-
 const buyProduct = async (req, res) => {
-    if (!selectedProduct) {
-        res.status(400).send("No product selected yet")
-    } else {
-        res.send(selectedProduct)
-        // Buy
-    }
+    console.log("Product bought:")
+    console.log(req.body)
+    res.send(req.body)
 }
 
 
@@ -71,6 +48,5 @@ module.exports = {
     getProduct,
     addProduct,
     queryProduct,
-    selectProduct,
     buyProduct
 }
