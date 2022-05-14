b85b2f7e382441efae901188ae5be17b85441f29
XanMarta <gmxancrazy24114@gmail.com>
2022-05-14 17:58:39 +0700

    complete basic FE interaction

--- server/public/js/index.js
+++ server/public/js/index.js
@@ -1,15 +1,7 @@
 
-import { show_data, setDataSelected } from "./view/control.js"
-import { enable_keyevent } from "./view/key.js"
-import { request } from "./axios.js"
+import { show_category } from "./menu.js";
 
 
 window.onload = async () => {
-    var datas = await request("get", "/api/products/get")
-    show_data(datas)
-    setDataSelected((index, data) => {
-        console.log(`Index: ${index}`)
-        console.log(data)
-    })
-    enable_keyevent()
+    await show_category()
 }
--- server/public/js/view/block.js
+++ server/public/js/view/block.js
@@ -1,5 +1,6 @@
 
 const controlBlock = document.getElementById("controlBlock")
+const resultField = document.getElementById("result")
 
 var blocks = []
 var numBlock = 0
@@ -11,6 +12,10 @@ function updateSelected(value) {
         blocks[selected].className = "block"
     }
     blocks[value].className = "selected"
+    blocks[value].scrollIntoView({
+        block: "center",
+        behavior: "smooth"
+    })
     selected = value
 }
 
@@ -19,7 +24,9 @@ export function spawnBlock(number, datas=[]) {
     numBlock = number
     var numData = datas.length
     controlBlock.innerHTML = ""
+    resultField.style.display = "none"
     blocks = []
+    selected = 0
     for (var i = 0; i < numBlock; i++) {
         var block = document.createElement("div")
         block.className = "block"
@@ -30,6 +37,12 @@ export function spawnBlock(number, datas=[]) {
         blocks.push(block)
     }
     updateSelected(0)
+}
+
+
+export function show_result(result) {
+    resultField.style.display = "block"
+    resultField.innerHTML = result
 }
 
 
