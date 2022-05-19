62860bb31f06cd18c55f10a4ae657f3977fd0565
XanMarta <gmxancrazy24114@gmail.com>
2022-05-19 22:51:02 +0700

    add voiceline for menu

--- server/public/js/menu/receipt.menu.js
+++ server/public/js/menu/receipt.menu.js
@@ -1,7 +1,7 @@
 
 import { categoryMenu, receiptMenu } from "./list.js";
 import { request } from "../util/axios.js"
-import { playInteract } from "../sound/sound.js";
+import { playInteract, playVoices } from "../sound/sound.js";
 
 
 receiptMenu.init = async () => {
@@ -11,7 +11,7 @@ receiptMenu.init = async () => {
     receiptMenu.block_data = [ "Quay lại" ]
     receiptMenu.voice_init = {
         id: `receipt_${product._id}`,
-        text: `mua thành công ${product.name}`
+        text: `bạn đã thành công ${product.name}. ấn phím thứ nhất hoặc thứ hai để quay về màn hình chính`
     }
 }
 
@@ -30,6 +30,18 @@ receiptMenu.on_voice = async (voice) => {
         playInteract()
         categoryMenu.back()
     }
+}
+
+receiptMenu.on_listen = async () => {
+    let voices = [{
+        id: "guide_receipt_1",
+        text: "bạn đã mua thành công. ấn phím đầu tiên hoặc phím thứ hai để quay lại màn hình chính"
+    }]
+    voices.push({
+        id: "guide_34",
+        text: "ấn phím thứ ba để nghe hướng dẫn. ấn phím thứ tư để dùng giọng nói"
+    })
+    playVoices(voices)
 }
 
 
